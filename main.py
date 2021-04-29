18e73ae9c9cc732127b50dc094ef820175945337
Aneesh Pushparaj <>
2021-04-29 15:42:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#EscapeFromTarkovTextGame
+
+print("Welcome to Escape from Tarkov Text Edition!")
+print("Enter the word instructions to read them, but if you don't need them enter the word no")
+
+instructions = str(input())
+if instructions == "instructions":
+  print("You are a character trying to scavenge for materials, like ")
