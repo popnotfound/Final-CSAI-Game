f435f03bf0448b8c4a642802a858ff6083adec3c
Aneesh Pushparaj <>
2021-04-29 16:00:08 +0000

    revised instructions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #EscapeFromTarkovTextGame
+
 import time
 
 print("Welcome to Escape from Tarkov Text Edition!")
@@ -10,10 +11,11 @@ instructions = str(input(" "))
 
 if instructions == "instructions":
   print(" ")
-  print("You are a character trying to scavenge for materials, like \nguns, ammunition, and many more things to help you level up \nyour hideout, which is where you live and store all the things \nyou find.")
+  print("You are a character trying to scavenge for materials, like \nguns, ammunition, and many more things to help you level up \nyour hideout, which is where you live and store all the things \nyou find. To find these types of items, type directions or try and look in things and eventually you will find the extract to exit back to your hideout. ")
   print(" ")
-  print("I will give you 30 seconds to read the instructions. ")
-  time.sleep(30)
+  print("I will give you 1 minute to read the instructions. ")
+  time.sleep(60)
+  print(" ")
+  print("Okay. Lets's get into the game!")
 elif instructions == "no":
   print("Let's get into the game!")
-
