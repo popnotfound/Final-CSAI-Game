110272c3ac3e48f1bc005ca0c541ef98fd515eb3
Aneesh Pushparaj <>
2021-04-29 15:55:01 +0000

    base instructions and explanations to liking

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 #EscapeFromTarkovTextGame
+import time
 
 print("Welcome to Escape from Tarkov Text Edition!")
-print("Enter the word instructions to read them, but if you don't need them enter the word no")
+print(" ")
+print("Enter the word instructions to read them, but if you don't \nneed them enter the word no.")
 
-instructions = str(input())
+print(" ")
+instructions = str(input(" "))
+
 if instructions == "instructions":
-  print("You are a character trying to scavenge for materials, like ")
+  print(" ")
+  print("You are a character trying to scavenge for materials, like \nguns, ammunition, and many more things to help you level up \nyour hideout, which is where you live and store all the things \nyou find.")
+  print(" ")
+  print("I will give you 30 seconds to read the instructions. ")
+  time.sleep(30)
+elif instructions == "no":
+  print("Let's get into the game!")
+
